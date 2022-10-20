d3b50345d4843eb0c54c618041085df6651cc38b
김승원 <danimartinwife@gimseuncBookPro.home>
2022-10-20 11:22:57 +0200

    wrote basic test

--- cypress/e2e/app.spec.cy.ts
+++ cypress/e2e/app.spec.cy.ts
@@ -1,6 +1,17 @@
 // @ts-ignore
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('show pages correctly', () => {
+  it('show character list', () => {
+    cy.visit('/');
+  });
+
+  it('show character detail', () => {
+    const [RANDOM_CHARACTER_ID_MIN, RANDOM_CHARACTER_ID_MAX] = [1, 30];
+    const randomCharacterId = Math.floor(Math.random() * (RANDOM_CHARACTER_ID_MAX - RANDOM_CHARACTER_ID_MIN) + 1);
+    cy.visit(`/${randomCharacterId}`)
+  });
+
+  it('show error page', () => {
+    const sampleInvalidUrl = '/hello/world';
+    cy.visit(sampleInvalidUrl);
+  });
+});
