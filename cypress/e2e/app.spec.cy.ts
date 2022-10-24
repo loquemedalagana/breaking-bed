2765822a0e1e684d1541eafa2bec22f9e940db0d
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-10-24 15:32:12 +0200

    fix cypress error

--- cypress/e2e/app.spec.cy.ts
+++ cypress/e2e/app.spec.cy.ts
@@ -2,16 +2,19 @@
 describe('show pages correctly', () => {
   it('show character list', () => {
     cy.visit('/');
+    cy.wait(3000);
   });
 
   it('show character detail', () => {
     const [RANDOM_CHARACTER_ID_MIN, RANDOM_CHARACTER_ID_MAX] = [1, 30];
     const randomCharacterId = Math.floor(Math.random() * (RANDOM_CHARACTER_ID_MAX - RANDOM_CHARACTER_ID_MIN) + 1);
     cy.visit(`/${randomCharacterId}`);
+    cy.wait(3000);
   });
 
   it('show error page', () => {
     const sampleInvalidUrl = '/hello/world';
     cy.visit(sampleInvalidUrl);
+    cy.wait(3000);
   });
 });
