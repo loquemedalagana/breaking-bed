1e7633513262a5251915acc8f24a308b3fba43d6
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-10-25 15:16:07 +0200

    fix app saga error

--- src/actions/appSaga.ts
+++ src/actions/appSaga.ts
@@ -1,22 +1,15 @@
-import { put, select, all, takeLatest, fork } from 'redux-saga/effects';
+import { all, fork, take } from 'redux-saga/effects';
 
 import { SAVE_ERROR_MESSAGE, RESET_ERROR_MESSAGE } from 'src/actions/appActions';
-import { selectAppState, AppState } from 'src/stores/appStore';
 
-export function* saveErrorMessage(): Generator {}
-
-export function* watchLSaveErrorMessage(): Generator {
-  yield takeLatest(SAVE_ERROR_MESSAGE, saveErrorMessage);
+export function* saveErrorMessage(): Generator {
+  yield take(SAVE_ERROR_MESSAGE);
 }
 
 export function* resetErrorMessage(): Generator {
-  const { error } = (yield select(selectAppState)) as AppState;
-  if (error === null) return;
-  put({
-    type: RESET_ERROR_MESSAGE,
-  });
+  yield take(RESET_ERROR_MESSAGE);
 }
 
 export default function* rootAppSaga(): Generator {
-  yield all([fork(watchLSaveErrorMessage), fork(resetErrorMessage)]);
+  yield all([fork(saveErrorMessage), fork(resetErrorMessage)]);
 }
